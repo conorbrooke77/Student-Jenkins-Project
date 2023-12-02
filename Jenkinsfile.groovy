ed58f72e057bfa2dce255556e462b853352a47c1
Conor Brooke <conor@gmail.com>
2023-12-02 23:43:17 +0000

    Add Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,6 @@ pipeline {
     tools {
         maven 'M3'
     }
-
     environment {
         JAVA_HOME = "C:\\Program Files\\Java\\jdk-21"
     }
@@ -22,19 +21,36 @@ pipeline {
                 // Using 'mvn clean package' to compile and package the code
                 bat 'mvn clean package -Dmaven.test.failure.ignore=true'
             }
+            post {
+                success {
+                    echo 'Build was successful.'
+                }
+                failure {
+                    echo 'Build failed. Check the logs.'
+                }
+            }
+        }
+
+        stage('Test') {
+            steps {
+                echo 'Running tests...'
+                bat 'mvn test'
+            }
+            post {
+                success {
+                    echo 'Tests were successful.'
+                }
+                failure {
+                    echo 'Tests failed. Check the logs.'
+                }
+            }
         }
 
     }
 
     post {
         always {
-            echo 'The build process has completed.'
-        }
-        success {
-            echo 'Build was successful.'
-        }
-        failure {
-            echo 'Build failed. Check the logs for details.'
+            echo 'The pipeline has completed successfully.'
         }
     }
 }
