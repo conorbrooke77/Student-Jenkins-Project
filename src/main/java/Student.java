0f84ec21e31dc13ba66b225cca56646ed5e3c70b
Conor Brooke <conor@gmail.com>
2023-12-03 11:41:39 +0000

    Add Jenkinsfile

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,9 +1,6 @@
-/**
- * Created by meudecc on 15/02/2017.
- */
 public class Student {
 
-    public enum AttendanceGrade {ABSENT, VERY_POOR, AVERAGE, GOOD, VERY_GOOD};
+    public enum AttendanceGrade {ABSENT, VERY_POOR, AVERAGE, GOOD, VERY_GOOD}
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
